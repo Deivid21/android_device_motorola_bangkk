f07d3ad0beb45b18436b5b158f8285ba9751212b
Michael Bestas <mkbestas@lineageos.org>
2024-06-29 16:13:50 +0300

    bangkk: Initial kernel build changes
    Change-Id: Ia8bb8fe65ffdfc249eceef0504eba52272ffa9db

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -13,7 +13,7 @@ TARGET_BOOTLOADER_BOARD_NAME := bangkk
 
 # Kernel
 BOARD_KERNEL_CMDLINE += androidboot.hab.product=bangkk
-TARGET_KERNEL_CONFIG += vendor/lineage_bangkk.config
+TARGET_KERNEL_CONFIG += vendor/ext_config/moto-holi-bangkk.config
 
 # Kernel Modules
 BOARD_VENDOR_KERNEL_MODULES_LOAD := $(strip $(shell cat $(DEVICE_PATH)/modules.load))
