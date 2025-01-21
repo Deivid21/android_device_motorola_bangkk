e26c9973d9d73e80952e2ce6024a57272bfc8461
Michael Bestas <mkbestas@lineageos.org>
2025-01-21 09:49:05 +0200

    [DNM] Use unified kernel config
    Change-Id: Ia52299c05a8c4d24a734287c0cb7bac969224f68

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -21,7 +21,7 @@ ODM_MANIFEST_N_FILES := $(DEVICE_PATH)/sku/manifest_n.xml
 
 # Kernel
 BOARD_KERNEL_CMDLINE += androidboot.hab.product=bangkk
-TARGET_KERNEL_CONFIG += vendor/ext_config/moto-holi-bangkk.config
+TARGET_KERNEL_CONFIG += vendor/bangkk_defconfig
 
 # Kernel Modules
 BOARD_VENDOR_KERNEL_MODULES_LOAD := $(strip $(shell cat $(DEVICE_PATH)/modules.load))
