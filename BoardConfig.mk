2db0841be47b71a708a37a1ed488eef843dc141c
Michael Bestas <mkbestas@lineageos.org>
2025-02-22 21:59:59 +0200

    bangkk: Drop device specific livedisplay manifest
    Change-Id: I7d912d79d1d3542357bef9a7bdb752ecf3860f08

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -11,9 +11,6 @@ include device/motorola/sm6375-common/BoardConfigCommon.mk
 # Bootloader
 TARGET_BOOTLOADER_BOARD_NAME := bangkk
 
-# HIDL
-DEVICE_MANIFEST_FILE += $(DEVICE_PATH)/manifest.xml
-
 # Kernel
 BOARD_KERNEL_CMDLINE += androidboot.hab.product=bangkk
 TARGET_KERNEL_CONFIG += vendor/lineage_bangkk.config
